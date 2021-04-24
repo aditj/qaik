657d7ff6846105131bfd10c313f74fc37ff30404
adit <aditjain1980@gmail.com>
2021-04-24 19:43:36 +0530

    made login,register and added trends

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Hashtag
+from .models import Hashtag,UserProfile
 
 admin.site.register(Hashtag)
+admin.site.register(UserProfile)
 # Register your models here.
--- main/views.py
+++ main/views.py
@@ -1,15 +1,17 @@
+from django.http import request
 from django.shortcuts import render,redirect
 from .forms import NewUserForm
 from django.contrib.auth.forms import AuthenticationForm 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login,authenticate,logout
 import re
-
+from django.contrib.auth.models import User
 from django.contrib import messages #import messages
 from .models import QAIT,UserProfile,Hashtag
 @login_required
 def feed(request):
 	trends=Hashtag.objects.all()
+
 	return render(request,"main/feed.html",context={
 		'trends':trends,
 	})
@@ -24,29 +26,32 @@ def login_view(request):
 	return render(request,"main/login.html")
 def register(request):
 	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			p=UserProfile(user=user)
-			p.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("/feed")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm
-	return render (request=request, template_name="main/register.html", context={"register_form":form})
+		user = User(username=request.POST['username'],password=request.POST['password'],email=request.POST['email'],first_name=request.POST['first_name'],last_name=request.POST['last_name'])
+		user.save()
+		p=UserProfile(user=user)
+		p.save()
+		login(request, user)
+		messages.success(request, "Registration successful." )
+		return redirect("/feed")
+	
+
+	return render (request=request, template_name="main/register.html")
 def logout_view(request):
 	logout(request)
 	return redirect("/login")
 def create_qait(request):
 	hashtags=re.findall("#(\w+)",request.POST["content"])
+	
 	for h in hashtags:
-		hashtag=Hashtag.objects.get_or_create(title=h)
+		hashtag=Hashtag.objects.get_or_create(title=h)[0]
+
 		hashtag.count= hashtag.count+1
 		hashtag.save()
 	p=UserProfile.objects.filter(user=request.user).first()
 	
-	q=QAIT(content=request.POST["content"],by=p)
+	q=QAIT(content=request.POST["content"],by=p,by_id=p.id)
 
 	q.save()
 	return redirect('/feed')
+def profile(request):
+	return render(request,"main/profile.html")
